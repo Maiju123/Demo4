a64ea20e94950b1e078aa762f44ba283f27eb635
Maiju Schreck <K2322@student.jamk.fi>
2016-02-08 17:03:06 +0200

    RadioApplication done

---
+++ RadioApplication/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //first radio
+            Radio radio1 = new Radio();
+            radio1.RadioOn = true;
+            Console.WriteLine("RADIO NUMBER 1:");
+            radio1.Volume = 0;
+            radio1.Frequency = 20405.100;
+            Console.WriteLine("Radio is on: {0}", radio1.RadioOn);
+            Console.WriteLine("Volume is: {0}", radio1.Volume);
+            Console.WriteLine("Station is: {0}", radio1.Frequency);
+
+
+            //space
+            Console.WriteLine(" ");
+            
+            //second radio
+            Radio radio2 = new Radio();
+            radio2.RadioOn = false;
+            Console.WriteLine("RADIO NUMBER 2:");
+            radio2.Volume = 2;
+            radio2.Frequency = 2000.1;
+            Console.WriteLine("Radio is on: {0}", radio2.RadioOn);
+            Console.WriteLine("Volume is: {0}", radio2.Volume);
+            Console.WriteLine("Station is: {0}", radio2.Frequency);
+
+            //space
+            Console.WriteLine(" ");
+
+            //third radio
+            Radio radio3 = new Radio();
+            radio3.RadioOn = true;
+            Console.WriteLine("RADIO NUMBER 3:");
+            radio3.Volume = 100;
+            radio3.Frequency = 1000.1;
+            Console.WriteLine("Radio is on: {0}", radio3.RadioOn);
+            Console.WriteLine("Volume is: {0}", radio3.Volume);
+            Console.WriteLine("Station is: {0}", radio3.Frequency);
+        }
+
+
+}
+}
---
+++ RadioApplication/Radio.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioApplication
+{
+    class Radio
+        // field variables
+    {
+        private readonly int minVolume = 0;
+        private readonly int maxVolume = 10;
+        private readonly double minFrequency = 2000.0;
+        private readonly double maxFrequency = 2600.0;
+        private int volume;
+        private double frequency;
+       
+
+        //default constructor
+        public Radio()
+        {
+        }
+
+        // Radio on or off
+       public bool RadioOn { get; set;} 
+
+        //Volume property
+        public int Volume {
+            get
+            {
+                return volume;
+            }
+            set {
+                if (value < minVolume)
+                {
+                    Console.WriteLine("Radio volume cant be turned on minus level. Radio volume has to be chosen between 1 and 10.");
+                    volume = value;
+                }
+                if (value > maxVolume)
+                {
+                    Console.WriteLine("TOO LOUD MUSIC, TURN IT DOWN! Radio volume has to be chosen between 1 and 10.");
+                    volume = value;
+                }
+                if (value == 0)
+                {
+                    Console.WriteLine("There is no sound in radio. Choose radio volune between 1 and 10.");
+                    volume = value;
+                } 
+                else
+                {
+
+                    Console.WriteLine("*Music on*");
+                    volume = value;
+                } 
+            }
+        }
+
+        //Frequence property
+        public double Frequency{
+            get
+            {
+                return frequency;
+            }
+            set {
+                if (value < minFrequency)
+                {
+                    Console.WriteLine("Radio works on frequencys between 2000.0 and 2600.0");
+                    frequency = value;
+                }
+                if (value > maxFrequency)
+                {
+                    Console.WriteLine("Radio works on frequencys between 2000.0 and 2600.0");
+                    frequency = value;
+                }
+                else
+                {
+                    Console.WriteLine("*Frequence chosen*");
+                    frequency = value;
+                }
+            }
+
+        }
+    }
+}
