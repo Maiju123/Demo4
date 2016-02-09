7ef20e11899e77ac16fc39aa7af5a7c207500eb8
Maiju Schreck <K2322@student.jamk.fi>
2016-02-09 16:41:23 +0200

    Homework_Book

---
+++ BookShelfApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShelfApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
